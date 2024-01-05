1fe135beb69612fa4cec6f73e6ade2d0017dcc91
Selina Pohl <selina.pohl355@gmail.com>
2024-01-05 12:03:58 +0100

    everything working

---
+++ ContinuousMC/Gillespie.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+//#include "array_alloc.h"
+
+
+void gillespie_step(double * times, double * x_var, double * y_var){
+    double x_curr= x_var[-1];
+
+}
+
+int main(){
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+
+}
